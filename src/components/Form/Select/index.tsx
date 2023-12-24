04b8901c0027f150032e9e58eace7b1e4200da4a
Rodrigo Rael <rodrigorael53@gmail.com>
2023-12-24 20:30:53 -0300

    :lipstick: feat: animations

--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -27,7 +27,7 @@ export function Select({ children, placeholder, ...props }: SelectProps) {
           side="bottom"
           position="popper"
           sideOffset={8}
-          className="z-10 w-[--radix-select-trigger-with] overflow-hidden rounded-lg border border-zinc-300 bg-white shadow-sm"
+          className="animate-slideDownAndFade z-10 w-[--radix-select-trigger-with] overflow-hidden rounded-lg border border-zinc-300 bg-white shadow-sm"
         >
           <SelectPrimitive.Viewport>{children}</SelectPrimitive.Viewport>
         </SelectPrimitive.Content>
